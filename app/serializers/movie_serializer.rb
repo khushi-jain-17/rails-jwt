f58b7d1700db0e728c86fb37238bf10f01ae01ce
khushi-jain-17 <khushisjain33@gmail.com@domain.com>
2023-08-11 19:06:32 +0530

    ser

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,6 +1,15 @@
 class MovieSerializer < ActiveModel::Serializer
 	
-  attributes :name, :rating, :description, :director, :released_on, :user_id, :category_id
+  attributes :description, :director, :released_on, :user_id, :category_id, :movie
+  
+  def movie
+    {
+      id: object.movie.id,
+      name: object.movie.name,
+      rating: object.rating
+    }
+  end
+
   belongs_to :user
   belongs_to :category
   has_many :reviews, dependent: :destroy
--- spec/requests/movie_spec.rb
+++ spec/requests/movie_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Movies", type: :request do
-  describe "GET /index" do
-  
+    before :each do
+    @movie = FactoryBot.create(:movie, name: "titanic",rating: 4,director: "sample",description:"sdescription")
+    @token = JsonWebToken.encode(movie_id: @movie.id)
+    allow(controller).to receive(:authorize_request)
+    @name = @movie.name
   end
+
+  describe "GET /movie" do
+    before { get '/movie', headers: { Authorization: @token } }
+
+    it 'returns users' do
+      expect(json).not_to be_empty
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+   end
+
+  
 end
+
